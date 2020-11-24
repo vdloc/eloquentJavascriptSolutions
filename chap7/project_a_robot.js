e8060cd281b144bffb844b31ed26e8281336d43f
vdloc <uvcudlco@gmail.com>
2020-11-24 23:19:04 +0700

    [ADD] add first pieces of project

---
+++ chap7/project_a_robot.js
@@ -0,0 +1,35 @@
+const roads = [
+  "Alice's House-Bob's House",
+  "Alice's House-Cabin",
+  "Alice's House-Post Office",
+  "Bob's House-Town Hall",
+  "Daria's House-Ernie's House",
+  "Daria's House-Town Hall",
+  "Ernie's House-Grete's House",
+  "Grete's House-Farm",
+  "Grete's House-Shop",
+  "Marketplace-Farm",
+  "Marketplace-Post Office",
+  "Marketplace-Shop",
+  "Marketplace-Town Hall",
+  "Shop-Town Hall",
+];
+
+const buildGraph = (roads) =>
+  roads.reduce((graph, road) => {
+    const [from, to] = road.split("-");
+
+    graph[from] = [...(graph[from] || []), to];
+    graph[to] = [...(graph[to] || []), from];
+
+    return graph;
+  }, Object.create(null));
+
+class VillageState {
+  constructor(place, parcels) {
+    this.place = place;
+    this.parcels = parcels;
+  }
+
+  move(destination) 
+}
